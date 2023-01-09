9445a4e4a480cbf958ba5692186f69bb50ed7a21
Malachi Coan <jmcoan4320@gmail.com>
2023-01-09 12:33:57 -0500

    Fixed ceasar function and added ascii art

--- src/Project1/Main.java
+++ src/Project1/Main.java
@@ -12,54 +12,40 @@ public class Main {
             'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
 
     public static void main(String[] args) {
-        // write your code here
-
         Scanner scnr = new Scanner(System.in);
+        CeasarCipherArt ca = new CeasarCipherArt();
         String direction;
         String text;
         int shift;
         String goAgain;
-//symbols = ['!','@','#','$','%','^','&','*','(',')','_','+','-','~','=']
-
         boolean end = false;
-        //print(art.logo)
+
+        System.out.println();
+        System.out.println(ca.logo);
+        System.out.println();
         while (!end) {
 
             System.out.println("Type 'encode' to encrypt, type 'decode' to decrypt:\n");
             direction = scnr.nextLine();
-            //System.out.println(direction);
 
             System.out.println("Type your message:\n");
             text = scnr.nextLine().toLowerCase(Locale.ROOT);
-
 
             System.out.println("Type the shift number:\n");
             shift = scnr.nextInt();
             scnr.nextLine();// this consumes the \n which is added when using scnr.nextInt()
 
-            //System.out.println(direction + text + shift);
-
-
             caesar(text, shift, direction);
             System.out.println("Do you want to keep going? Type 'yes' or 'no'. ");
-            //System.out.println("Print me ");
             goAgain = scnr.nextLine();
-            //System.out.println("Print me too");
 
             if (goAgain.equalsIgnoreCase("no")) {
-                //System.out.println("It was a no");
                 end = true;
 
             }
 
         }
 
-        /* def caesar(start_text, shift_amount, cipher_direction):
-
-
-
-
-         */
     }
 
     private static void caesar(String text, int shift, String direction) {
@@ -70,18 +56,20 @@ public class Main {
         }
         if (direction.equalsIgnoreCase("decode")) {
             shift *= -1;
-            for (int i = 0; i >= text.length() - 1; i++) {
+        }
+            for (int i = 0; i <= text.length() - 1; i++) {
 
                 if (!alphabet.contains(text.charAt(i))) {
                     end_text += text.charAt(i);
                 } else {
                     int position = alphabet.indexOf(text.charAt(i));
                     int new_position = position + shift;
-                    end_text += alphabet.get(new_position);
-                    System.out.println(alphabet.get(new_position));
+                    int idx = (alphabet.size() + new_position) % alphabet.size(); //this is how to simulate a negative index
+
+                    end_text += alphabet.get(idx);
                 }
             }
-        }
+
         System.out.println("Here's the " + direction + "d result: " + end_text);
     }
 
